1be01fa58cef3fb70a5a43d409ccca118a849b58
Roman <63646879+Romnasi@users.noreply.github.com>
2020-07-14 16:03:33 +0300

    Добавляет js для промо-слайдера

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+// Variables for popup-map
 let mapLink = document.querySelector('.map-popup-link');
 let mapPopup = document.querySelector('.modal-map');
 let mapClose = mapPopup.querySelector('.modal-close');
 
+// Variables for popup-form
 let formButton = document.querySelector('.write-us-btn');
 let formPopup = document.querySelector('.write-us');
 let formClose = formPopup.querySelector('.modal-close');
@@ -10,6 +12,17 @@ let nameField = formPopup.querySelector('[name=name]');
 let emailField = formPopup.querySelector('[name=email]');
 let textField = formPopup.querySelector('[name=text]');
 
+// Variables for Promo-slider
+let promoButtons = document.querySelector('.promo-slider-controls');
+let promoButtonFirst = promoButtons.querySelector('.promo-slider-button-first');
+let promoButtonSecond = promoButtons.querySelector('.promo-slider-button-second');
+let promoButtonThird = promoButtons.querySelector('.promo-slider-button-third');
+let promoSliders = document.querySelector('.promo-slider-list');
+let promoSliderFirst = promoSliders.querySelector('.promo-slider-first');
+let promoSliderSecond = promoSliders.querySelector('.promo-slider-second');
+let promoSliderThird = promoSliders.querySelector('.promo-slider-third');
+
+// Variables for localStorage
 let isStorageSupport = true;
 let storage = '';
 
@@ -79,5 +92,61 @@ window.addEventListener("keydown", function (evt) {
       formPopup.classList.remove("modal-show");
       formPopup.classList.remove('modal-error');
     }
+  };
+});
+
+// Promo-slider
+promoButtonFirst.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if (promoButtonFirst.classList.contains("promo-slider-button-active")) {
+    formPopup.offsetWidth = formPopup.offsetWidth;
+    promoSliderFirst.classList.remove('hide-slide');
+  } else {
+    promoButtonSecond.classList.remove('promo-slider-button-active');
+    promoButtonThird.classList.remove('promo-slider-button-active');
+    promoButtonFirst.classList.add('promo-slider-button-active');
+  };
+
+  if (promoSliderFirst.classList.contains('hide-slide')) {
+    promoSliderFirst.classList.remove('hide-slide');
+    promoSliderSecond.classList.add('hide-slide');
+    promoSliderThird.classList.add('hide-slide');
+
+  }
+});
+
+
+promoButtonSecond.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if (promoButtonSecond.classList.contains("promo-slider-button-active")) {
+
+  } else {
+    promoButtonFirst.classList.remove('promo-slider-button-active');
+    promoButtonThird.classList.remove('promo-slider-button-active');
+    promoButtonSecond.classList.add('promo-slider-button-active');
+  };
+
+  if (promoSliderSecond.classList.contains('hide-slide')) {
+    promoSliderSecond.classList.remove('hide-slide');
+    promoSliderFirst.classList.add('hide-slide');
+    promoSliderThird.classList.add('hide-slide');
+  }
+});
+
+
+promoButtonThird.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if (promoButtonThird.classList.contains("promo-slider-button-active")) {
+
+  } else {
+    promoButtonFirst.classList.remove('promo-slider-button-active');
+    promoButtonSecond.classList.remove('promo-slider-button-active');
+    promoButtonThird.classList.add('promo-slider-button-active');
+  };
+
+  if (promoSliderThird.classList.contains('hide-slide')) {
+    promoSliderThird.classList.remove('hide-slide');
+    promoSliderFirst.classList.add('hide-slide');
+    promoSliderSecond.classList.add('hide-slide');
   }
 });
